14abda0d7f43ed3902e4c8cb877e23d9561de8cf
Henare Degan <henare.degan@gmail.com>
2015-03-25 17:25:31 +1100

    Ruby all the things

--- scraper.rb
+++ scraper.rb
@@ -1,6 +1,15 @@
 require "resolv"
+require "mechanize"
+require "net/telnet"
 
 puts "IP address for morph.io is:"
 puts Resolv::DNS.new.getaddress("morph.io")
-puts "$ netcat india.colorado.edu 13"
-puts `netcat india.colorado.edu 13`
+
+puts "Name of a search engine:"
+puts Mechanize.new.get("http://google.com").title
+
+puts "Get BOFH excuse via telnet"
+tn = Net::Telnet::new("Host" => "towel.blinkenlights.nl", "Port" => 666)
+puts tn.waitfor("Match" => "BOFH")
+
+puts "Done."
