d5ba38e125e44968698dc0f43571231b4bd76da0
Henare Degan <henare.degan@gmail.com>
2015-03-25 17:02:47 +1100

    Use Ruby to do a DNS lookup

--- scraper.rb
+++ scraper.rb
@@ -1,4 +1,6 @@
-#puts "$ host morph.io"
-#puts `host morph.io`
+require "resolv"
+
+puts "IP address for morph.io is:"
+puts Resolv::DNS.new.getaddress("morph.io")
 puts "$ netcat india.colorado.edu 13"
 puts `netcat india.colorado.edu 13`
